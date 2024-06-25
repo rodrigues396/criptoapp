10088d611680c71f6917dfb98474a0483f083c76
rodrigues396 <thallisonsiqueira@gmail.com>
2024-06-25 09:49:26 -0300

    Revert "feat: alteration style"
    This reverts commit bfd21c1d532ed3c31375da0a95cb4cf64702a90f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
-import RoutesApp from './routes';
-import './App.css';
 
+import './App.css'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <RoutesApp/>
-    </BrowserRouter>
+    <>
+    </>
   )
 }
 
-export default App;
+export default App
